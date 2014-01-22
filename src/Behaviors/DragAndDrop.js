04668b28f2fbdb19aeaed820eb015535d4a877bb
Greyson Parrelli <greyson.parrelli@gmail.com>
2014-01-22 12:33:00 -0500

    Cleaned up DragAndDrop.

--- src/Behaviors/DragAndDrop.js
+++ src/Behaviors/DragAndDrop.js
@@ -1,45 +1,52 @@
-//Drag and Drop behavior
 this.cutie = this.cutie || {};
 
 (function(module){
+    /**
+     * This behavior makes the object able to be dragged and
+     * dropped with the mouse/touch.
+     * @param {Object} props
+     *        <Description of properties.>
+     */
     var DragAndDrop = function(props) {
-        this.props = props;
+        // ================================================
+        // VARIABLE DECLARATIONS
+        // ================================================
+        var _isDragging = false;
+        var _clickOffset = {};
+
+        // ================================================
+        // PUBLIC METHODS
+        // ================================================
+        this.init = function(obj) {
+            obj.addEventListener('mousedown', mousedown.bind(this, obj), false);
+            obj.addEventListener('pressup', mouseup.bind(this, obj), false);
+        }
+
+        this.clean = function(obj) {
+        }
+
+        this.tick = function(obj) {
+            if(_isDragging) {
+                var stage = cutie.getStage();
+                obj.x = stage.mouseX - _clickOffset.x;
+                obj.y = stage.mouseY - _clickOffset.y;
+            }
+        }
+
+        // ================================================
+        // PRIVATE METHODS
+        // ================================================
+        function mousedown(obj, e) {
+            _isDragging = true;
+            _clickOffset.x = e.stageX - obj.x;
+            _clickOffset.y = e.stageY - obj.y;
+        }
+
+        function mouseup(obj, e) {
+            _isDragging = false;
+        }
     };
 
-    DragAndDrop.prototype._isDragging = false;
-    DragAndDrop.prototype._clickOffset = {};
-
-    DragAndDrop.prototype.init = function(obj) {
-        obj.addEventListener('mousedown', mousedown.bind(this, obj), false);
-        obj.addEventListener('pressup', mouseup.bind(this, obj), false);
-    }
-
-    DragAndDrop.prototype.clean = function(obj) {
-    }
-
-    DragAndDrop.prototype.tick = function(obj) {
-        if(this._isDragging) {
-            var stage = cutie.getStage();
-            obj.x = stage.mouseX - this._clickOffset.x;
-            obj.y = stage.mouseY - this._clickOffset.y;
-        }
-    }
-
-    function mousedown(obj, e) {
-        this._isDragging = true;
-        this._clickOffset.x = e.stageX - obj.x;
-        this._clickOffset.y = e.stageY - obj.y;
-    }
-
-    function mouseup(obj, e) {
-        this._isDragging = false;
-    }
-
+    // Copy our behavior into the module
     module.DragAndDrop = DragAndDrop;
 })(this.cutie);
-
-/*
-document.addEventListener('keyup', function(obj) {
-                return function(e) {upLeftRight(e, obj); };
-            } (this) , false);
-*/
