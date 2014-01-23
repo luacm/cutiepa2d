3965e7d302b334eed892c1787d1cbc7f0f8fbc29
Greyson Parrelli <greyson.parrelli@gmail.com>
2014-01-23 08:55:11 -0500

    Added phone example.

---
+++ examples/phone-simple/scenes/TitleScene.js
@@ -0,0 +1,21 @@
+(function() {
+    var scene = new cutie.Scene();
+
+    scene.preload = function(loader) {
+        loader.loadFile("assets/spiderman-ball.png");
+    }
+
+    scene.init = function() {
+        cutie.Log.d("title.init()");
+
+        for (var i = 0; i < 64; i++) {
+            var spidey = new cutie.Bitmap("assets/spiderman-ball.png");
+            spidey.x = Math.random() * 550 + 10;
+            spidey.y = Math.random() * 850 + 10;
+            this.addChild(spidey);     
+            spidey.addBehavior(new cutie.DragAndDrop());
+        }
+    }
+
+    cutie.registerScene(scene, "title");
+})();
